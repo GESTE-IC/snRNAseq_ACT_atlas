fa585dc85c6a0e6e38f8223de8431d28c24fc7d3
GESTE-IC <anne.jouinot@inserm.fr>
2025-05-23 17:55:01 +0200

    Update and rename ACC2_plots.R to Plots.R

--- Spatial_transcriptomics/Plots.R
+++ Spatial_transcriptomics/Plots.R
@@ -3,6 +3,8 @@ library(ggplot2)
 library(patchwork)
 library(viridis)
 
+### ACC2
+
 ACC2 <- readRDS("/path/to/results/ACC2_annot.RDS")
 
 figure_dir <- "/path/to/figures/"
@@ -41,3 +43,33 @@ SpatialFeaturePlot(ACC2, features = c("GM1_ZG_1", "GM2_ZF1_2", "GM3_ZF2_3", "GM4
         legend.title = element_text(size=30),
         legend.text = element_text(size=30), 
         legend.key = element_blank())
+
+
+### NAd
+
+NAd <- readRDS("/path/to/results/nadintegrated_annot.RDS")
+
+figure_dir <- "/path/to/figures/"
+sample_name <- "NAd_integrated_"
+
+clustree(nadintegrated)
+
+SpatialDimPlot(nadintegrated, group.by = "integrated_snn_res.0.2", crop = FALSE, pt.size.factor = 1.2, image.alpha = 0) & 
+  scale_fill_viridis_d(option = "turbo") & 
+  ggtitle("") &
+  theme(legend.key.size = unit(2, "cm"),
+        legend.title = element_text(size=30),
+        legend.text = element_text(size=30), 
+        legend.key = element_blank()) &
+  guides(fill = guide_legend(override.aes = list(size = 20))) &
+  labs(fill = "Clusters res 0.2")
+
+my.nadintegrated.cols <- c("Capsule" = "#7A0403FF", "ZG" = "#E4460AFF", "ZF" = "#A2FC3CFF", "ZF HSP+" = "#30123BFF", "ZR" = "#4686FBFF", "ZR / Medulla" = "#1AE4B6FF", "Medulla / ZR - Macrophages enriched" = "#FABA39FF")
+SpatialDimPlot(nadintegrated, group.by = "new.ident.clusters", crop = FALSE, pt.size.factor = 1.2, image.alpha = 0, cols = my.nadintegrated.cols) & 
+  ggtitle("") &
+  theme(legend.key.size = unit(2, "cm"),
+        legend.title = element_text(size=30),
+        legend.text = element_text(size=30), 
+        legend.key = element_blank()) &
+  guides(fill = guide_legend(override.aes = list(size = 20))) &
+  labs(fill = "")
